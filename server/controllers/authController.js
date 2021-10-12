3cde57ede0b767f96e8c909cde4c050663037b82
theprashil <prashil_silwal@protonmail.com>
2021-10-12 09:57:59 +0545

    Added user login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,8 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-// 1 Sign up new user
 exports.signup = catchAsync(async (req, res, next) => {
   const result = await User.create({
     name: req.body.name,
@@ -17,4 +17,29 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 });
 
-//2 Login user
+exports.login = catchAsync(async (req, res, next) => {
+  //chech if the email and password is there in req
+  const { email, pass } = req.body;
+
+  if (!email || !pass)
+    return next(new AppError('Enter email and password', 404));
+
+  // validate the cred
+  const user = await User.findOne({ email: email }).select('+password');
+
+  if (!user || !(await user.correctPass(pass, user.password)))
+    return next(new AppError('Email address or password is invalid', 404));
+
+  //generate token and pass to the client
+  const token = jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30d',
+    }
+  );
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
