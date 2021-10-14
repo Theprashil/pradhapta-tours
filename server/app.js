7d6cc827b14839a774197f4ca436e094f481f7e2
theprashil <prashil_silwal@protonmail.com>
2021-10-14 15:01:48 +0545

    Added some security measures

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 
@@ -9,11 +14,39 @@ const userRoute = require('./routes/userRoute');
 const globalErrorHandler = require('./controllers/errorController');
 
 //Middlewares
+
+//sets security headers
+app.use(helmet());
+
+// logs request in console
 if (process.env.ENV === 'Dev') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json()); //body parser
+// Limit req from the same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from the ip. Please try again in an hour',
+});
+
+app.use('/api', limiter);
+
+//body parser
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against nosql query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevents parameter pollution
+app.use(
+  hpp({
+    whitelist: ['duration'], //will add more later on
+  })
+);
 
 //Routes
 app.use('/api/tours', tourRoute);
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -43,6 +43,16 @@ exports.login = catchAsync(async (req, res, next) => {
       expiresIn: '30d',
     }
   );
+
+  const cookieOptions = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  if (process.env.ENV === 'prod') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(200).json({
     status: 'success',
     token,
